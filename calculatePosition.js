4fc1345117c492b092b242f2e1760d3ea2b5ba92
SoniaDumitru <soni.dumitru@gmail.com>
2020-12-02 14:46:37 -0600

    Update calculatePosition.js
    split tests, fixed method signature to accept an obj as argument, used Math.round at the end, implemented Math.pow instead of the second loop

--- calculatePosition.js
+++ calculatePosition.js
@@ -1,17 +1,18 @@
-const calculatePosition = (startingPosition, daysWaiting, numberOfReferrals) => {
+const calculatePosition = ({startingPosition, daysWaiting, numberOfReferrals}) => {
     let finalPosition = 0;
     let referralPosition = 0;
+    // if there are no referrals position becomes startingPosition
     if (numberOfReferrals === 0) {
-        finalPosition = startingPosition;
+      finalPosition = startingPosition;
     }
+    // if there are referrals each time we iterate we calculate and then subtract the position worth by the number of referrals
     for (let i = 1; i <= numberOfReferrals; i++) {
-        referralPosition = referralPosition + Math.round(1000/i);
-        finalPosition = startingPosition - referralPosition;
+      referralPosition = referralPosition + 1000/i;
+      finalPosition = startingPosition - referralPosition;
     }
-    for (let j = 1; j <= daysWaiting; j++) {
-        finalPosition = finalPosition - Math.round(0.0143 * finalPosition);
-    } 
-    return finalPosition;
+    // finally we calculate position based on referrals and also by daysWaiting
+    finalPosition = finalPosition * Math.pow(1 - 0.0143, daysWaiting);
+  return Math.round(finalPosition);
 }
 
 module.exports = calculatePosition;
