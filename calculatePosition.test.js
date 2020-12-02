99ba5e265f5450cfc7817933f4dd18fec94cffec
SoniaDumitru <soni.dumitru@gmail.com>
2020-12-02 14:47:28 -0600

    Update calculatePosition.test.js

--- calculatePosition.test.js
+++ calculatePosition.test.js
@@ -2,12 +2,21 @@ const calculatePosition = require('./calculatePosition.js');
 
 describe("Calculate Position", () => {
     test("it should calculate waitlist position based on referrals and how many days they have been waiting", () => {
-        expect(calculatePosition(10000,0,0)).toBe(10000); // 0 referrals 0 days
-        expect(calculatePosition(10000,0,1)).toBe(9000); // 1 referral
-        expect(calculatePosition(10000,0,2)).toBe(8500); // 2 refferals
-        expect(calculatePosition(10000,0,3)).toBe(8167); // 3 referrals
-        expect(calculatePosition(10000,1,0)).toBe(9857); // 0 referrals and 1 day
-        expect(calculatePosition(10000,2,1)).toBe(8744); // 0 referrals and 2 days
-        expect(calculatePosition(10000,100,1)).toBe(2134); // 0 referrals and 2 days
+        expect(calculatePosition({ startingPosition: 10000, daysWaiting: 0,  numberOfReferrals: 0 })).toBe(10000); // 0 referral
+    });
+    test("it should return waitlist position of 8500 if 2 referrals and 0 days waiting" , () => {
+        expect(calculatePosition({ startingPosition: 10000, daysWaiting: 0,  numberOfReferrals: 2 })).toBe(8500); // 2 referrals
+    });
+    test("it should return waitlist position of 8167 if 3 referrals and 0 days waiting" , () => {
+        expect(calculatePosition({ startingPosition: 10000, daysWaiting: 0,  numberOfReferrals: 3 })).toBe(8167); // 3 referrals
+    });
+    test("it should return waitlist position of 8167 if 0 referrals and 1 days waiting" , () => {
+        expect(calculatePosition({ startingPosition: 10000, daysWaiting: 1,  numberOfReferrals: 0 })).toBe(9857); // 0 referrals and 1 day
+    });
+    test("it should return waitlist position of 8167 if 1 referrals and 2 days waiting" , () => {
+        expect(calculatePosition({ startingPosition: 10000, daysWaiting: 2,  numberOfReferrals: 1 })).toBe(8744); // 1 referrals and 2 days
+    });
+    test("it should return waitlist position of 8167 if 1 referrals and 2 days waiting" , () => {
+        expect(calculatePosition({ startingPosition: 10000, daysWaiting: 100,  numberOfReferrals: 1 })).toBe(2132); // 1 referrals and 100 days    
     });
 });
